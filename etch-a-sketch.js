08f467ec6bdd437651010fecb4e30b31166691e5
Bryan <bryan.how@live.com>
2021-05-27 21:44:57 +0800

    add css grid to div container

---
+++ etch-a-sketch.js
@@ -0,0 +1,3 @@
+// add grid display and grids to the container div
+const container = document.querySelector(".container");
+container.style.cssText = "display: grid; grid-template-rows: repeat(16, 1fr); grid-template-columns: repeat(16, 1fr)";
